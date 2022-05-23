2475c7e805af5fe8042baf323b9d407f2712f01b
Adriano Silva <ampsilva19rgmail.com>
2022-05-23 20:22:01 +0100

    [add] unit testing for lipid maps transformer

--- tests/unit_tests/test_etl.py
+++ tests/unit_tests/test_etl.py
@@ -1,11 +1,19 @@
 import unittest
-from boimmgpy.etl.lipid_maps import LipidMapsExtractor
+from boimmgpy.etl.lipid_maps import LipidMapsExtractor,LipidMapsTransformer
+from boimmgpy.etl.swiss_lipids import SwissLipidsExtractor
 
 class TestETL(unittest.TestCase):
-
-    def test_lipid_maps_extract():
+    def test_lipid_maps_extract(self):
         extractor = LipidMapsExtractor()
         df = extractor.extract()
+
+    def test_swiss_lipids_extract(self):
+        extractor = SwissLipidsExtractor()
+        df = extractor.extract()
+
+    def test_lipid_maps_transformer(self):
+        transformer=LipidMapsTransformer()
+        df_treted=transformer.transform()
 
 if __name__ == '__main__':
     unittest.main()
