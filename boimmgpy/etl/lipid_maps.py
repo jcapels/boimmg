d988181fe458281590022565cc3839285aaa258d
Adriano Silva <ampsilva19rgmail.com>
2022-05-23 20:22:50 +0100

    [add] lipid maps transformer

--- boimmgpy/etl/lipid_maps.py
+++ boimmgpy/etl/lipid_maps.py
@@ -6,7 +6,8 @@ from airflow.models.dag import dag
 from airflow.operators.python import PythonOperator
 import requests, zipfile, io
 
-from airflow_interfaces import AirflowExtractor, AirflowTransformer, AirflowLoader, AirflowPipeline
+
+from boimmgpy.etl.airflow_interfaces import AirflowExtractor, AirflowTransformer, AirflowLoader, AirflowPipeline
 
 
 class LipidMapsExtractor(AirflowExtractor):
@@ -22,14 +23,14 @@ class LipidMapsExtractor(AirflowExtractor):
         return self._extract(self.scrape_data())
 
 
-    def _extract(self, raw, **kwargs) -> pd.DataFrame:
+    def _extract(self, raw) -> pd.DataFrame:
         """
         Method to create a pandas dataframe with the scraped data.
         :return: pandas data frame of lipid maps data 
         """
         raw=PandasTools.LoadSDF(raw)
         df=pd.DataFrame(raw)
-        
+        #print(df.head())
         return df
 
     def scrape_data(self):
@@ -37,21 +38,67 @@ class LipidMapsExtractor(AirflowExtractor):
         This class downloads the ZIP file and extracts the sdf file of lipid maps.
         :return: raw sdf file of lipid maps data
         """
-        r = requests.get("https://www.lipidmaps.org/files/?file=LMSD&ext=sdf.zip")
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-    
-        return z.open('structures.sdf')  
+        raw_file = requests.get("https://www.lipidmaps.org/files/?file=LMSD&ext=sdf.zip")
+        file_unziped = zipfile.ZipFile(io.BytesIO(raw_file.content))
+        return file_unziped.open('structures.sdf')  
 
 
 
 class LipidMapsTransformer(AirflowTransformer):
 
-    def transform(self, **kwargs):
+    def __init__(self) -> None:
+        self.treated_dataframe=pd.DataFrame(columns=['LM_ID','ABBREVIATION','SYNONYMS'])
+
+    def transform(self):
         """
         This method allows to transform the dataframe previously extracted into a desired datastructure
         :return:
         """
-        pass
+        data=LipidMapsExtractor()
+        data_treated=self.treat(data.extract())
+        return data_treated
+    
+    def three_col_lm(self,data_three):
+        df_three_col=data_three[['LM_ID','ABBREVIATION','SYNONYMS']]
+        return df_three_col
+
+    def treat(self,data):
+        df=self.three_col_lm(data)
+        first_col=df[['LM_ID']].values
+        second_col=df[['ABBREVIATION']].values
+        third_col=df[['SYNONYMS']].values
+
+        for n in range (len(first_col)): #len(first_col)
+            abrevs=second_col[n]
+            synonyms=third_col[n]
+            lista_ab=[]
+            lista_sy=[]
+            lista_id=[]
+            if abrevs != None:
+                for value in abrevs:
+                    value=str(value)
+                    value=value.split(';')
+                    for i in value:
+                        lista_ab.append(i)
+
+            if synonyms != None:    
+                for value in synonyms:
+                    value=str(value)
+                    value=value.split(';')
+                    for i in value:
+                        lista_sy.append(i)
+
+            for a in range(max([len(lista_ab),len(lista_sy)])):
+                for value in first_col[n]:
+                    value=str(value)
+                    lista_id.append(value)
+
+            # pegar em cada linha e adicionar ao df
+            treated_data=pd.DataFrame.from_dict({'LM_ID': lista_id, 'ABBREVIATION': lista_ab, 'SYNONYMS':lista_sy},orient='index').T
+            #df.append(treated_data,ignore_index=True,)
+            self.treated_dataframe=pd.concat([self.treated_dataframe,treated_data],ignore_index=True,sort=False,axis=0)
+        print(self.treated_dataframe.head())
+        return self.treated_dataframe
 
 
 class LipidMapsLoader(AirflowLoader):
@@ -66,7 +113,7 @@ class LipidMapsLoader(AirflowLoader):
 
 class LipidMapsETLPipeline(AirflowPipeline):
 
-    @task  # add parameters to decorator eventually
+    #@task  # add parameters to decorator eventually
     def extract(self, **kwargs):
         """
         Method where the extraction method will be added.
@@ -74,7 +121,7 @@ class LipidMapsETLPipeline(AirflowPipeline):
         extractor = LipidMapsExtractor()
         extractor.extract(**kwargs)
 
-    @task  # add parameters to decorator eventually
+    #@task  # add parameters to decorator eventually
     def transform(self, **kwargs):
         """
         Method where the transform method will be added.
@@ -82,7 +129,7 @@ class LipidMapsETLPipeline(AirflowPipeline):
         transformer = LipidMapsTransformer()
         transformer.transform(**kwargs)
 
-    @task  # add parameters to decorator eventually
+    #@task  # add parameters to decorator eventually
     def load(self, **kwargs):
         """
         Method where the load method will be added.
