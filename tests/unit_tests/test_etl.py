faa5543db76af7c9e77e93f0182e6d41a41fb996
AdrianoSilva19 <ampsilva19@gmail.com>
2022-07-01 02:04:49 +0100

    [FIX] Unit test structure

--- tests/unit_tests/test_etl.py
+++ tests/unit_tests/test_etl.py
@@ -15,12 +15,11 @@ class TestLipidMapsETL(unittest.TestCase):
         self.treated_dataframe = self.transformer.transform(self.scrape_data)
 
     def test_lipid_maps_extract(self):
-        self.assertEqual(self.scrape_data.shape[1],23) 
+        df=self.scrape_data
+        self.assertEqual(df.shape[1],23) 
 
-    def test_lipid_maps_transformer(self):
         self.assertEqual(self.treated_dataframe.shape[1],2)
     
-    def test_lipid_maps_loader(self):
         self.loader.load(self.treated_dataframe)
 
 
@@ -34,16 +33,10 @@ class TestSwissLipidsETL(unittest.TestCase):
         self.treated_dataframe = self.transformer.transform(self.scrape_data)
 
     def test_swiss_lipids_extract(self):
-        df = self.extractor.extract()
+        df = self.scrape_data
         self.assertEqual(df.shape[1],29) 
-
-    def test_swiss_lipids_transformer(self):
         self.assertEqual(self.treated_dataframe.shape[1],2)
-        
-        
-    def test_lipid_maps_loader(self):
         self.loader.load(self.treated_dataframe)
-
 
 
 if __name__ == '__main__':
