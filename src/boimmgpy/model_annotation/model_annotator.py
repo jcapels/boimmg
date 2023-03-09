2b01cfb423c0cbd9873fe725a83d569d51852508
AdrianoSilva19 <ampsilva19@gmail.com>
2023-03-09 15:43:57 +0000

    [FIX] multiprocessing

--- src/boimmgpy/model_annotation/model_annotator.py
+++ src/boimmgpy/model_annotation/model_annotator.py
@@ -1,16 +1,17 @@
-from cobra.io import read_sbml_model
 import re
 from tqdm import tqdm
 from collections import defaultdict
+from collections import Counter
 from boimmgpy.database.accessors.database_access_manager import DatabaseAccessManager
 from joblib import Parallel, delayed
-
-
-driver = DatabaseAccessManager(conf_file_path="my_database.conf").connect()
-session = driver.session()
+from typing import List
+from cobra import Model,Metabolite
 
 
 class LipidNameAnnotator:
+    """
+    Class that given a model implements the annotation of the structurally defined lipids in that model.
+    """
     def __init__(self) -> None:
         self.backbone = None
         self.converted_lipid_class_dict = {}
@@ -19,31 +20,62 @@ class LipidNameAnnotator:
         self.counter = {}
 
 
+    def login(self):
+        """Method to create the connection to the database
 
-    def treat_data(self,dict_list):
-        res = {}
+        Returns:
+            _type_: _description_
+        """
+        driver = DatabaseAccessManager(conf_file_path="my_database.conf").connect()
+        session = driver.session()
+        return session
+
+    def treat_data(self,dict_list:List[tuple]):
+        """Method responsible for handling the dictionary fractions originating from the multiprocessing of the medel_lipids_finder method.
+         Here all partitions are assigned to a corresponding final dictionary.
+
+        Args:
+            dict_list (List[tuple]): List of tuples containing 4 dictionaries in each tuple
+        """
         
         for converted_lipid_class_dict, check_annotation_dict, counter, results in dict_list:
             if isinstance(converted_lipid_class_dict, defaultdict):
-                converted_lipid_class_dict = dict(converted_lipid_class_dict)
+                converted_lipid_class_dict = Counter(converted_lipid_class_dict)
             if isinstance(counter, defaultdict):
-                counter = dict(counter)
+                counter = Counter(counter)
 
-            
-
-            self.converted_lipid_class_dict.update(converted_lipid_class_dict)
+            self.counter = {i: self.counter.get(i, 0) + counter.get(i, 0)
+            for i in set(self.counter).union(counter)}
+            self.converted_lipid_class_dict = {i: self.converted_lipid_class_dict.get(i, 0) + converted_lipid_class_dict.get(i, 0)
+            for i in set(self.converted_lipid_class_dict).union(converted_lipid_class_dict)}
+        
             self.check_annotation_dict.update(check_annotation_dict)
-            self.counter.update(counter)
             self.results.update(results)
 
 
-    def model_lipids_finder(self,model):
+    def model_lipids_finder(self,model:Model):
+        """
+        Method responsible for multiprocessing the metabolites of the model, thus accelerating the whole annotation procedure.
+
+        Args:
+            model (Model): Cobrapy metabolic model
+
+        Returns:
+            _type_: A tuple with four dictionaries first one refers to the extent of the lipid classes present in the model, 
+            the second one refers to the pre-presence of annotation on the lipids, 
+            third for the extent of the classes that the algorithm was able to annotate 
+            and finally a dictionary with the identifiers in the model for the lipids and their identifier in the Lipid Maps and/or Swiss Lipids database
+        """
         n_iterations = len(model.metabolites)
         parallel_callback = Parallel(-1)
         resultados = parallel_callback(delayed(self._model_lipids_finder)(model.metabolites[i]) for i in tqdm(range(n_iterations)))
         self.treat_data(resultados)
-        print(self.converted_lipid_class_dict)
-        return resultados 
+        return (
+            self.converted_lipid_class_dict,
+            self.check_annotation_dict,
+            self.counter,
+            self.results,
+        )
 
     def _model_lipids_finder(self,metabolite):
         """Method that searchs for lipid metabolites in model and finds their synonyms in the BOIMMG database
@@ -107,24 +139,29 @@ class LipidNameAnnotator:
             check_annotation_dict[lipid.id] = annotated
         return check_annotation_dict
 
-    def search_lipid_synonyms(self, metabolite,side_chain):
+    def search_lipid_synonyms(self, metabolite:Metabolite,side_chain:List[str]):
         """Method that implements the screening in the database for accurate lipid structure.
         This method calls all StaticMethods to do the screening in the database.
 
         Args:
-            metabolite (_type_): Lipid metabolit of the model.
+            metabolite (Metabolite): Given Metabolite of the cobra model
+            side_chain (List[str]): List of the side chains referent to the given Metavolite
+
+        Returns:
+            _type_: tuple with two dictionaries, first one for the extend of the lipid classes that the algorithm caugth and second one for the identifiers to do the annotation
         """
+        session = self.login()
         results={}
         counter = defaultdict(int)
         is_compound = False
-        lipid_id = self.get_synonym_id(self.backbone, side_chain)
+        lipid_id = self.get_synonym_id(session,self.backbone, side_chain)
         if lipid_id != None and not lipid_id[2]:
             for backbone in lipid_id[0]:
-                lipid_compound = self.get_coumpound(backbone, lipid_id[1], is_compound)
+                lipid_compound = self.get_coumpound(session,backbone, lipid_id[1], is_compound)
                 if lipid_compound != None and lipid_compound[0] != 0:
                     structurally_defined_lipids = (
                         self.get_compounds_with_specific_parent_set_of_components(
-                            lipid_compound[0], lipid_compound[1]
+                        session,lipid_compound[0], lipid_compound[1]
                         )
                     )
                     if structurally_defined_lipids:
@@ -138,9 +175,9 @@ class LipidNameAnnotator:
                         counter[self.backbone] += 1
 
                 if lipid_compound != None and lipid_compound[0] == 0:
-                    backbone_compound = self.get_compound_from_synonym(backbone)
+                    backbone_compound = self.get_compound_from_synonym(session,backbone)
                     structurally_defined_lipids = (
-                        self.get_compounds_with_specific_parent_set_of_components(
+                        self.get_compounds_with_specific_parent_set_of_components(session,
                             backbone_compound, lipid_compound[1]
                         )
                     )
@@ -157,11 +194,11 @@ class LipidNameAnnotator:
         if lipid_id != None and lipid_id[2] == True:
             is_compound = True
             for backbone in lipid_id[0]:
-                lipid_compound = self.get_coumpound(backbone, lipid_id[1], is_compound)
+                lipid_compound = self.get_coumpound(session,backbone, lipid_id[1], is_compound)
                 if lipid_compound != None:
                     structurally_defined_lipids = (
                         self.get_compounds_with_specific_parent_set_of_components(
-                            lipid_compound[0], lipid_compound[1]
+                            session,lipid_compound[0], lipid_compound[1]
                         )
                     )
                     if structurally_defined_lipids:
@@ -176,7 +213,7 @@ class LipidNameAnnotator:
         return counter,results
     
     @staticmethod
-    def get_synonym_id(backbone: str, side_chain: list):
+    def get_synonym_id(session,backbone: str, side_chain: list):
         """Method that searches for the synonyms of the backbone and side chains for each lipid in the model.
         In cases where the synonym for the backbone doesnt exist it will search for the generic compound with the same name as the backbone:
 
@@ -231,7 +268,7 @@ class LipidNameAnnotator:
                 return backbone_id, sidechain_id, compound
 
     @staticmethod
-    def get_coumpound(backbone: str, sidechains: list, flag: bool):
+    def get_coumpound(session,backbone: str, sidechains: list, flag: bool):
         """Method that search for the the specific compounds attached for the side chain and backbone synonym's.
         In case that the ID from the backbone is already from a compound it will search only for the side chains compounds.
 
@@ -277,7 +314,7 @@ class LipidNameAnnotator:
                 return backbone, side_chains_coumpound
 
     @staticmethod
-    def get_compound_from_synonym(synonym_ID: int):
+    def get_compound_from_synonym(session,synonym_ID: int):
         """Method to get the generic compound directly connected to the backbone synonnym.
 
         Args:
@@ -297,7 +334,7 @@ class LipidNameAnnotator:
 
     @staticmethod
     def get_compounds_with_specific_parent_set_of_components(
-        parent: str, components: list
+        session,parent: str, components: list
     ):
         """Get all non genericall compounds with the specific set of components
 
@@ -326,4 +363,9 @@ class LipidNameAnnotator:
 
         if len(res) != 0:
             return res
+        
+
+    def set_annotation():
+        
+        pass
 
--- src/boimmgpy/model_annotation/statistic_info.py
+++ src/boimmgpy/model_annotation/statistic_info.py
@@ -1,14 +1,16 @@
 from boimmgpy.model_annotation.model_annotator import LipidNameAnnotator
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from cobra.io import read_sbml_model
 
 
 def get_info(path):
-    model = read_sbml_model(path)
-    #metabolite_names = [metabolite.name for metabolite in model.metabolites]
+    """ Function that gets all statisticall information from LipidNameAnnotator class relative to lipids class caugth and number of lipids annotated.
+    This data is stored in a spreadsheet specific for each model analised.
 
+    Args:
+        path (_type_): Path to the model to be analised
+    """
+    model = read_sbml_model(path)
     annotator = LipidNameAnnotator()
     info = annotator.model_lipids_finder(model)
     model_id = model.id
@@ -21,23 +23,15 @@ def get_info(path):
     original_annotations = pd.DataFrame(original_annotations)
     class_annotated = pd.Series(info[2])
     class_annotated = pd.DataFrame(class_annotated)
-    #annotations = pd.Series(info[3])
+    print(class_annotated)
 
     with pd.ExcelWriter(path) as writer:
-        # to store the dataframe in specified sheet
         lipids_class.to_excel(writer, sheet_name=str(model_id), index=True, startrow=0 , startcol=0)
         original_annotations.to_excel(writer, sheet_name=str(model_id), index=True, startrow=0 , startcol=8)
         class_annotated.to_excel(writer, sheet_name=str(model_id), index=True,startrow=0, startcol=4)
 
-"""
-    print(lipids_class)
-    print(sum(lipids_class.values()))
-    print(len(original_annotations))
-    print(sum(map((True).__eq__, original_annotations.values())))
-"""
-
 if __name__ == '__main__':
-    get_info(r"models\iBD1106.xml")
-    #get_info(r"models\chorella\PP2016-00593DR3_Data1Model_Heterotrophy.xml")
-    #get_info(r"models\12918_2017_441_MOESM3_ESM.xml")
-    #get_info(r"models\iLB1027_lipid.xml")
+    get_info(r"models/iBD1106.xml")
+    get_info(r"models\chorella\PP2016-00593DR3_Data1Model_Heterotrophy.xml")
+    get_info(r"models\12918_2017_441_MOESM3_ESM.xml")
+    get_info(r"models\iLB1027_lipid.xml")
