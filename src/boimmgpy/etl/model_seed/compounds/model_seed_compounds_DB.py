e455ecba60aa5c196a0d4de468512531f62074ed
AdrianoSilva19 <ampsilva19@gmail.com>
2023-03-22 21:25:35 +0000

    [ADD] Method to set the structures db of Model Seed compounds

--- src/boimmgpy/etl/model_seed/compounds/model_seed_compounds_DB.py
+++ src/boimmgpy/etl/model_seed/compounds/model_seed_compounds_DB.py
@@ -1,6 +1,7 @@
-
+from rdkit.Chem.rdmolfiles import MolFromSmiles, MolToSmiles
 from boimmgpy.etl.model_seed.compounds.ModelSeedCompound import ModelSeedCompound
 from boimmgpy.etl.model_seed.compounds.model_seed_db_scraper import ModelSeedCompoundsDBScraper,ModelSeedStructuresDBScraper
+from boimmgpy.utilities import chemo_utilities
 
 
 class ModelSeedCompoundsDB:
@@ -9,9 +10,14 @@ class ModelSeedCompoundsDB:
         self.__inchikey_database = {}
         self.__smiles_database = {}
         self.__compounds = []
+        self._set_compounds_db()
+        
+
+    def _set_compounds_db(self):
         self.read_model_seed_compounds()
-
-
+        self.read_model_seed_structures()
+        self.set_inchi_key_and_smiles_database()
+    
     def get_compound_by_id(self, id):
         return self.__compounds_database[id]
 
@@ -39,10 +45,25 @@ class ModelSeedCompoundsDB:
         return None
 
 
-    def read_model_seed_structures():
+    def read_model_seed_structures(self):
         extractor = ModelSeedStructuresDBScraper()
         structure_dataframe = extractor.extract()
-    
+        mapped_features = {}
+        previous_id = structure_dataframe['ID'].iloc[0]
+        for i,row in structure_dataframe.iterrows():
+            _id = row["ID"]
+            if previous_id != _id:
+                smiles = mapped_features["smile"]
+                if "inchikey" in mapped_features.keys():
+                        inchikey = mapped_features["inchikey"]
+
+                if "inchi" in mapped_features.keys():
+                    inchi = mapped_features["inchi"]
+
+                self.__compounds_database[previous_id].setStructures(smiles, inchikey, inchi)
+                mapped_features = {}
+            mapped_features[row["Type"].lower()] = row["Structure"].strip()
+            previous_id = _id
 
     def read_model_seed_compounds(self):
         self.__compounds_database = {}
@@ -61,6 +82,40 @@ class ModelSeedCompoundsDB:
             self.__compounds.append(modelseed_compound)
 
 
+    def set_inchi_key_and_smiles_database(self):
+        self.isomer_smiles_database = {}
+        for compound in self.__compounds:
+            inchikey = compound.getInchikey()
+            smiles = compound.getSmiles()
+        if inchikey or smiles:
+                self.__inchikey_database[inchikey[:-1]] = compound
+                mol = MolFromSmiles(smiles)
+
+                if mol:
+                    canonical_smiles_not_isomer = MolToSmiles(mol, isomericSmiles=False)
+                    canonical_smiles = MolToSmiles(mol)
+
+                    try:
+                        neutralized, _ = chemo_utilities.neutralise_charges(canonical_smiles)
+                        neutralized_not_isomer, _ = chemo_utilities.neutralise_charges(canonical_smiles_not_isomer)
+
+                        if neutralized in self.__smiles_database:
+                            self.__smiles_database[neutralized].append(compound)
+
+                        else:
+                            self.__smiles_database[neutralized] = [compound]
+
+                        if neutralized_not_isomer in self.isomer_smiles_database:
+                            self.isomer_smiles_database[neutralized_not_isomer].append(compound)
+
+                        else:
+                            self.isomer_smiles_database[neutralized_not_isomer] = [compound]
 
 
-ModelSeedCompoundsDB()
+                    except:
+                        pass
+
+
+if __name__ == '__main__':
+    teste = ModelSeedCompoundsDB()
+    teste._set_compounds_db()
