f2461770b94ad788e08a265065e2993ad5baae8e
AdrianoSilva19 <ampsilva19@gmail.com>
2023-03-30 15:14:17 +0100

    [ADD] multiprocessing to the integration of lipid maps database in neo4j database

--- src/boimmgpy/etl/lipid_maps/lipid_maps_db.py
+++ src/boimmgpy/etl/lipid_maps/lipid_maps_db.py
@@ -1,3 +1,5 @@
+from typing import List
+from joblib import Parallel, delayed
 import pandas as pd
 from tqdm import tqdm
 from boimmgpy.database.accessors.compounds_database_accessor import CompoundsDBAccessor
@@ -7,27 +9,64 @@ from boimmgpy.database.databases_babel import BOIMMGDatabases
 from rdkit.Chem.rdmolfiles import MolFromSmiles, MolToSmiles, MolFromSmarts
 
 
+                        
+class Wrapper(object):
+    """Wrapper class to get functions that cannot be picklized
+
+    Args:
+        object (_type_): _description_
+    """
+    def __init__(self, method_name, module_name):
+        self.method_name = method_name
+        self.module_name = module_name
+
+    def __call__(self, *args, **kwargs):
+        try:
+            method = __import__(self.module_name, globals(), locals(), [self.method_name,])
+            _function = getattr(method, self.method_name)
+            result = _function(*args, **kwargs)
+            return result
+        except Exception as e:
+            print('Error calling function:', self.method_name)
+            print('Module:', self.module_name)
+            print('Error message:', str(e))
+            return None
+
+    
 
 class LipidMapsDB:
-
-    def treat_dataframe():
-        data_base_connection = GraphDatabase.driver(uri="bolt://localhost:7687",auth=("neo4j","potassio19"))
-        session = data_base_connection.session()
+    def treat_dataframe(self):
+        """Method that iterates through all lipid maps lipids dictionary and gives it to insert_lm_database method be inserted in a neo4j database.
+        This process is done using joblib multiprocessing 
+        """
         lipids_db = LipidMapsStructureDB()
         lipid_maps_db = lipids_db.getDatabase()
+        parallel_callback = Parallel(-3)
+        data_treated = parallel_callback(delayed(self.insert_lm_database)(value) for key,value in tqdm(lipid_maps_db.items()))
+        data_treated = pd.concat(data_treated)
+        
+        
+    def insert_lm_database(self,lipid_container:List):
+        """Method that receives a lipid container, acesses its information and sets a new node for each lipid with all relevant information
+
+        Args:
+            lipid_container (_type_): Lipid container 
+        """
+        driver = GraphDatabase.driver(uri="bolt://localhost:7687",auth=("neo4j","potassio19"))
         accessor = CompoundsDBAccessor()
-        for lipid in tqdm(lipid_maps_db):
-            lipid_container = lipid_maps_db[lipid]
-            smiles = lipid_container.getSmiles()
-            if pd.isna(smiles):
+        smiles = lipid_container.getSmiles()
+        if pd.isna(smiles):
+            canonical_smiles = None
+        else:
+            try:
+                mol_from_smiles = Wrapper("MolFromSmiles","rdkit.Chem.rdmolfiles" )
+                mol = mol_from_smiles(smiles)
+                mol_to_smiles = Wrapper("MolToSmiles","rdkit.Chem.rdmolfiles" )
+                canonical_smiles = mol_to_smiles(mol)
+            except:
                 canonical_smiles = None
-            else:
-                try:
-                    canonical_smiles = MolToSmiles(MolFromSmiles(smiles))
-                except:
-                    canonical_smiles = None
-            aliases = lipid_container.getAliases()
-            chebi_id = None
+        aliases = lipid_container.getAliases()
+        chebi_id = None
         lipid_bank_id = None
         pubchem_id = None
         hmdb_id = None
@@ -61,15 +100,15 @@ class LipidMapsDB:
             if not res:
 
                 with driver.session() as session:
-                    session.run("MERGE (c:Compound { lipidmaps_id: $lipid_maps_id}) "
+                    session.run("MERGE (c:Compound:LipidMapsCompound { lipidmaps_id: $lipid_maps_id}) "
                                 "ON CREATE SET c.lipid_maps_id = $lipid_maps_id, "
                                 "c.smiles = $smiles, c.generic = False, c.inchikey = $inchikey, "
                                 "c.formula = $formula, c.charge = 0, c.kegg_id = $kegg_id, c.inchi = $inchi, "
                                 "c.chebi_id = $chebi_id, c.lipid_bank_id = $lipid_bank_id,"
                                 "c.name = $name,"
-                                "c.pubchem_cid = $pubchem_id, c.hmdb_id = $hmdb_id, c.time_stamp = timestamp() "
+                                "c.pubchem_cid = $pubchem_id, c.hmdb_id = $hmdb_id "
                                 ,
-                                lipid_maps_id=lipid,
+                                lipid_maps_id=lipid_container.getDbId(),
                                 smiles=canonical_smiles,
                                 inchikey=lipid_container.getInchiKey(),
                                 formula=lipid_container.getFormula(),
@@ -80,16 +119,16 @@ class LipidMapsDB:
                                 )
 
             else:
-                to_merge_node = res[0]
-                if BOIMMGDatabases.LIPID_MAPS.value not in to_merge_node.aliases.keys():
+                if BOIMMGDatabases.LIPID_MAPS.value not in res.aliases.keys():
                     with driver.session() as session:
                         session.run("MATCH (c:Compound) "
                                     "where id(c) = $ont_id "
                                     "set c.lipid_maps_id = $lipid_maps_id,"
                                     "c.kegg_id = $kegg_id",
-                                    ont_id=to_merge_node.id,
+                                    ont_id=res.id,
                                     lipid_maps_id=lipid_container.getDbId(),
                                     kegg_id=kegg_id)
+
 
 if __name__ == "__main__":
     set = LipidMapsDB()
