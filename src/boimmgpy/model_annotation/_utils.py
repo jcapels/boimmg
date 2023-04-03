5b0cd4552fd4cfc816b494689ceacd35cfb67202
AdrianoSilva19 <ampsilva19@gmail.com>
2023-04-03 17:08:10 +0100

    [FIX] variable names

--- src/boimmgpy/model_annotation/_utils.py
+++ src/boimmgpy/model_annotation/_utils.py
@@ -15,11 +15,11 @@ def set_metabolite_annotation_in_model(session:GraphDatabase.driver,dictionary_r
     Returns:
         Model: Gsm model with defined Lipids annotated
     """
-    for key,value in dictionary_results.items():
+    for metabolite_ids,boimmg_ids in dictionary_results.items():
         lipid_maps_ids=[]
         swiss_lipids_ids=[]
-        for id in value:
-            result=session.run("match(c:Compound)where id(c)=$boimmg_id return c.lipidmaps_id,c.swiss_lipids_id as ids", boimmg_id=id)
+        for boimmg_id in boimmg_ids:
+            result=session.run("match(c:Compound)where id(c)=$boimmg_id return c.lipidmaps_id,c.swiss_lipids_id as ids", boimmg_id=boimmg_id)
             data= result.data()
             for node in data:
                 node_lipid_maps_id = node.get("c.lipidmaps_id")
@@ -29,8 +29,9 @@ def set_metabolite_annotation_in_model(session:GraphDatabase.driver,dictionary_r
                 node_swiss_lipids_id = node.get("ids")
                 if node_swiss_lipids_id!= None:
                     swiss_lipids_ids.append(node_swiss_lipids_id)
+        
         for metabolite in model.metabolites:
-            if metabolite.id == key:    
+            if metabolite.id == metabolite_ids:    
                 if len(lipid_maps_ids) != 0:
                     for values in lipid_maps_ids:
                         metabolite.annotation["lipidmaps"] = values
