6b8e8cd11297f5ce0da7c7ffb102e3cc0c94e31c
AdrianoSilva <ampsilva19@gmail.com>
2023-04-08 10:31:06 +0100

    [ADD] Lipid maps set db to the pipeline

--- pipelines/synonyms_etl/etl_pipeline.py
+++ pipelines/synonyms_etl/etl_pipeline.py
@@ -1,7 +1,12 @@
+from boimmgpy.etl.lipid_maps.lipid_maps_db import LipidMapsDB
 from boimmgpy.etl.lipid_maps.lipid_maps_synonyms import LipidMapsExtractor, LipidMapsTransformer, LipidMapsLoader
 from boimmgpy.etl.swiss_lipids.swiss_lipids_synonyms import SwissLipidsExtractor, SwissLipidsTransformer, SwissLipidsLoader
 
 
+
+def run_set_lipid_maps_db():
+    set = LipidMapsDB()
+    set.treat_dataframe()
 
 def run_lm_pipeline():
     """ This method implements the ETL pipeline for Lipid Maps dataset 
@@ -27,5 +32,6 @@ def run_sl_pipeline():
 
 
 if __name__ == '__main__':
+    run_set_lipid_maps_db()
     run_lm_pipeline()
     run_sl_pipeline()
