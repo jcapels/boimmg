b5829b60d39a4990fc6edd8a9b359abc748b4529
AdrianoSilva <ampsilva19@gmail.com>
2023-04-14 13:56:50 +0100

    [FIX] code structure

--- src/boimmgpy/etl/_utils.py
+++ src/boimmgpy/etl/_utils.py
@@ -4,7 +4,6 @@ from neo4j import GraphDatabase
 from boimmgpy.database.accessors.database_access_manager import DatabaseAccessManager
 
 
-
 def insert_in_database_lipid_maps(df: pd.Series):
     """
     This method creates the queries necessary to upload the treated data into the database
@@ -24,7 +23,9 @@ def insert_in_database_lipid_maps(df: pd.Series):
             session.run(
                 "match (l:Compound),(s:Synonym) where l.lipidmaps_id=$lipid_maps_id and s.synonym=$synonym "
                 "merge (s)-[:is_synonym_of]->(l)",
-                synonym=lm_synonym, lipid_maps_id=lipid_maps_id)
+                synonym=lm_synonym,
+                lipid_maps_id=lipid_maps_id,
+            )
 
 
 def insert_in_database_swiss_lipids(df: pd.Series):
@@ -35,7 +36,7 @@ def insert_in_database_swiss_lipids(df: pd.Series):
     :return: List of queries necessary to the upload of the whole dataframe
     :rtype: list
     """
-    
+
     driver = DatabaseAccessManager(conf_file_path="my_database.conf").connect()
 
     with driver.session() as session1:
@@ -43,8 +44,16 @@ def insert_in_database_swiss_lipids(df: pd.Series):
             swiss_lipids_id = row["Lipid ID"]
             sl_synonym = row["Synonym"]
             session1.run('MERGE (s: Synonym {synonym:"%s"})' % str(sl_synonym))
-            session1.run("match (l:SwissLipidsCompound),(s:Synonym) where l.swiss_lipids_id=$sl_id and "
-                         "s.synonym=$synonym merge (s)-[:is_synonym_of]->(l)", synonym=sl_synonym,
-                         sl_id=swiss_lipids_id)
+            session1.run(
+                "match (l:SwissLipidsCompound),(s:Synonym) where l.swiss_lipids_id=$sl_id and "
+                "s.synonym=$synonym merge (s)-[:is_synonym_of]->(l)",
+                synonym=sl_synonym,
+                sl_id=swiss_lipids_id,
+            )
 
 
+"""CALL apoc.periodic.iterate(
+  "match (c:Compound) where c.swiss_lipids_id is not null return c",
+  "merge (c)<-[l:is_db_link_of]-(lm:SwissLipidsCompound {swiss_lipids_id: c.swiss_lipids_id})",
+  {batchSize:10000, parallel:true})
+"""
