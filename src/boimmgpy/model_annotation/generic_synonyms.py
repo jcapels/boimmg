386007b803fef9b1935cd49fb499d5a2ad95e455
AdrianoSilva19 <ampsilva19@gmail.com>
2023-04-20 14:54:33 +0100

    [FIX] change generics synonyms

--- src/boimmgpy/model_annotation/generic_synonyms.py
+++ src/boimmgpy/model_annotation/generic_synonyms.py
@@ -6,8 +6,8 @@ def set_compound_synonyms():
     """Insert synonyms in the genericall compounds to allow the proper work of the annotator class
     """
     driver = DatabaseAccessManager(conf_file_path="my_database.conf").connect()
-    synonym_list=["1,2-diacyl-sn-glycerol","cardiolipin","triacylglycerol","1-phosphatidyl-1d-myo-inositol3-phosphate","1-phosphatidyl-1d-myo-inositol4-phosphate"]
-    compound_list=["1,2-diacyl-sn-glycerol","Cardiolipins","Triacylglycerols","1,2-diacyl-sn-glycero-3-phospho-1D-myo-inositol-3-phosphate","1,2-diacyl-sn-glycero-3-phospho-1D-myo-inositol-4-phosphate"]
+    synonym_list=["triacylglycerol","1,2-diacyl-sn-glycerol","1-phosphatidyl-1d-myo-inositol3-phosphate","1-phosphatidyl-1d-myo-inositol4-phosphate","1,2-diacyl-3-o-galactosyl-sn-glycerol","digalactosyl-diacylglycerol","monogalactosyldiacylglycerol","digalactosyldiacylglycerol"]
+    compound_list=["triacyl-sn-glycerol","1,2-diacyl-sn-glycerol","1,2-diacyl-sn-glycero-3-phospho-1D-myo-inositol-3-phosphate","1,2-diacyl-sn-glycero-3-phospho-1D-myo-inositol-4-phosphate","1,2-diacyl-3-beta-D-galactosyl-sn-glycerol","1,2-diacyl-3-O-[alpha-D-galactosyl-(1->6)-beta-D-galactosyl]-sn-glycerol","1,2-diacyl-3-beta-D-galactosyl-sn-glycerol","1,2-diacyl-3-O-[alpha-D-galactosyl-(1->6)-beta-D-galactosyl]-sn-glycerol"]
     with driver.session() as session1:
         for i in range(len(synonym_list)):
             lipid_compound = compound_list[i]
